6680820b1a6a943ddbae5dd61a25379b7b5049ea
jywkim <jkim589@gmail.com>
2018-02-19 19:20:19 -0500

    move rotate method into Player class

--- player.js
+++ player.js
@@ -43,15 +43,34 @@ class Player {
   rotate(dir) {
     const pos = this.pos.x
     let offset = 1;
-    rotate(this.matrix, dir);
+    this._rotateMatrix(this.matrix, dir);
     while (collide(arena, this)) {
       this.pos.x += offset;
       offset = -(offset + (offset > 0 ? 1 : -1));
       if (offset > this.matrix[0].length) {
-        rotate(this.matrix, -dir);
+        this._rotateMatrix(this.matrix, -dir);
         this.pos.x = pos;
         return;
       }
+    }
+  }
+
+  _rotateMatrix(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+      for (let x = 0; x < y; ++x) {
+        [
+          matrix[x][y],
+          matrix[y][x],
+        ] = [
+          matrix[y][x],
+          matrix[x][y],
+        ];
+      }
+    }
+    if (dir > 0) {
+      matrix.forEach(row => row.reverse());
+    } else {
+      matrix.reverse();
     }
   }
 
--- tetris.js
+++ tetris.js
@@ -119,25 +119,6 @@ function merge(arena, player) {
   });
 }
 
-function rotate(matrix, dir) {
-  for (let y = 0; y < matrix.length; ++y) {
-    for (let x = 0; x < y; ++x) {
-      [
-        matrix[x][y],
-        matrix[y][x],
-      ] = [
-        matrix[y][x],
-        matrix[x][y],
-      ];
-    }
-  }
-  if (dir > 0) {
-    matrix.forEach(row => row.reverse());
-  } else {
-    matrix.reverse();
-  }
-}
-
 let lastTime = 0;
 function update(time = 0) {
   const deltaTime = time - lastTime;
